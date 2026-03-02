dbae85904d717d3d7402cbb2785ded1e13d1ac2a
Cornerstone <cornerstone.ros@gmail.com>
2026-03-02 00:39:55 +0000

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
