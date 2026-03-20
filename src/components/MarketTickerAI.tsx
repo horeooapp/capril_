e5bf0e60273f6d3d26cc5239de599b7539b243d2
Horeoo <horeoo.app@gmail.com>
2026-03-20 10:45:19 +0000

    UPDATE 1045

--- src/components/MarketTickerAI.tsx
+++ src/components/MarketTickerAI.tsx
@@ -4,10 +4,11 @@ import NewsTicker from './NewsTicker'
 
 export default async function MarketTickerAI() {
     const marketItems = await getMarketAIInsights()
-
+    
     return (
-        <div className="fixed bottom-20 xl:bottom-0 left-0 right-0 z-[100]">
+        <div className="fixed bottom-0 left-0 right-0 z-[99999] bg-red-600">
             <NewsTicker items={marketItems} duration={60} />
+            <div className="absolute top-0 right-0 bg-white text-black text-[10px] p-1">DEBUG: TICKER ACTIVE</div>
         </div>
     )
 }
